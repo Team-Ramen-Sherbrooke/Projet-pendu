d9520693ddb77cd6ff624c31060bcf805fb2ff27
Meuleuh <32850732+Meuleuh@users.noreply.github.com>
2019-11-20 10:43:05 -0500

    Ajout du gabarit

---
+++ jeu_pendu.cpp
@@ -0,0 +1,43 @@
+/****************************************************************************/
+/*                                                                          */
+/* Projet.................: Manipulation de fichiers                        */
+/* Programme..............: fichier_multilingue.cpp                         */
+/* Effectué pour..........: 420-135-SH_Introduction_à_la_programmation      */
+/*                                                                          */
+/* Auteur.................: Camélia Groleau                                 */
+/*                          Vincent Doucet                                  */
+/*                          David Marcoux                                   */
+/*                                                                          */
+/* Date de conception.....: 20 novembre 2019                                */
+/* Date de mise en service: -- -------- ----                                */
+/*                                                                          */
+/* Modifications                                                            */
+/* Date:             Par:                Justification:                     */
+/*                                                                          */
+/*                                                                          */
+/****************************************************************************/
+/*                                                                          */
+/* En entrée.: -Ce programme va initialement choisir un mot de façon        */
+/*              aléatoire dans sa banque de mot qui est un fichier          */
+/*              externe. Par la suite, il va interagir avec le client       */
+/*              selon les règles du jeu du pendu                            */
+/*                                                                          */
+/*--------------------------------------------------------------------------*/
+/*                                                                          */
+/* Traitement: -Ce programme va ouvrir un fichier externe contenant les     */
+/*              textes des diverses questions à poser à l'utilisateur       */
+/*              dans plusieurs langues et va sélectionner les questions     */
+/*              dans la langue correspondante au choix de l'usager. Par     */
+/*              la suite, il va stocker ces données dans un fichier         */
+/*              externe lui aussi.                                          */
+/*                                                                          */
+/*--------------------------------------------------------------------------*/
+/*                                                                          */
+/* En sortie.: -Ce programme va stocker les données utilisateurs dans un    */
+/*              fichier externe                                             */
+/*                                                                          */
+/****************************************************************************/
+
+/*************************************/
+/* Espace de nom                     */
+/*************************************/
