6528adaf39c61e74d1e20506fc577a3ee7cdd731
Meuleuh <32850732+Meuleuh@users.noreply.github.com>
2019-11-21 12:56:10 -0500

    Update jeu_pendu.cpp

--- jeu_pendu.cpp
+++ jeu_pendu.cpp
@@ -45,6 +45,35 @@
 /* Variables globales                */
 /*************************************/
   char vgl_affichage[23][80];
+  bool vgl_lettresChoisies[26] =
+  {
+    false, //a
+    false, //b
+    false, //c
+    false, //d
+    false, //e
+    false, //f
+    false, //g
+    false, //h
+    false, //i
+    false, //j
+    false, //k
+    false, //l
+    false, //m
+    false, //n
+    false, //o
+    false, //p
+    false, //q
+    false, //r
+    false, //s
+    false, //t
+    false, //u
+    false, //v
+    false, //w
+    false, //x
+    false, //y
+    false  //z
+  }
 
 /*************************************/
 /* Constantes                        */
